ee19e88bc054962f3233ee8ebcd932ef37d73e64
acedragneel <abhilashgp222@gmail.com>
2023-02-23 04:57:49 -0500

    Added the ec2 instance

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,6 +22,31 @@ variable "private_subnet_count" {
   type = number
 }
 
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+
+}
+
+variable "instance_volume_size" {
+  type    = number
+  default = 50
+}
+
+variable "app_port" {
+  type    = number
+  default = 8000
+}
+
+variable "ami_id" {
+  type    = string
+}
+
+variable "instance_volume_type" {
+  type    = string
+  default = "gp2"
+}
+
 locals {
   public_subnets  = [for i in range(1, var.public_subnet_count + 1) : cidrsubnet(var.vpc_cidr_block, 8, i)]
   private_subnets = [for i in range(1, var.private_subnet_count + 1) : cidrsubnet(var.vpc_cidr_block, 8, i + var.private_subnet_count)]

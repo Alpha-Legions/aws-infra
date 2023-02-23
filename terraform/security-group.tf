bd510784ce11557df6d312899d4e81f7cc40aa33
acedragneel <abhilashgp222@gmail.com>
2023-02-23 16:54:32 -0500

    Final Update

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -1,3 +1,8 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "ec2-ub"
+  public_key = var.ssh-key
+}
+
 resource "aws_security_group" "security_group" {
   name = "application"
   depends_on = [
@@ -33,4 +38,15 @@ resource "aws_security_group" "security_group" {
   }
 
   vpc_id = aws_vpc.vpc.id
+}
+
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  description = "Allow SSH traffic from the key-pair"
+  source_security_group_id = aws_security_group.security_group.id
 }

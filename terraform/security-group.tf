dfb9dfa88945e09a5a037548fe0c62bce1539a28
acedragneel <abhilashgp222@gmail.com>
2023-04-06 00:24:09 -0400

    Added the Load Balancer

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -1,15 +1,5 @@
-resource "aws_security_group" "security_group" {
-  name = "application"
-  depends_on = [
-    aws_vpc.vpc
-  ]
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group" "load_balancer_sg" {
+  name = "load_balancer"
 
   ingress {
     from_port   = 80
@@ -25,25 +15,40 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = var.app_port
-    to_port     = var.app_port
-    protocol    = "tcp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_security_group" "security_group" {
+  name = "application"
+  depends_on = [
+    aws_vpc.vpc
+  ]
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load_balancer_sg.id]
+  }
+
+  ingress {
+    from_port       = var.app_port
+    to_port         = var.app_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load_balancer_sg.id]
   }
 
   egress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -62,3 +67,4 @@ resource "aws_security_group" "db" {
     security_groups = [aws_security_group.security_group.id]
   }
 }
+
